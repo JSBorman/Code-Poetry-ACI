615588aa78fb3eaf072b44c607de57e2dc630938
Staethe <jzmynb@gmail.com>
2017-11-12 19:54:41 -0500

    Finished functions

--- Purpose.py
+++ Purpose.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import time
+import random
 
 this_year = 0
 i_live = True
 inspired = True
 still_searching = True
 chance = 0		# Needs to not be a constant
+health = .8
 
 my_purpose = 'tbd'
 life = ["find love", "honor", "earn a doctorate", "go to college"] # Include more life goals
@@ -31,19 +33,21 @@ def this_life(destiny):
 def keep_searching():
 	next_year = this_year + 1
 
-	# Check if still alive
-	if()
+	if still_alive() is False:
+		i_live = False
+	else:
+		i_live = True
 
 	#if(inspired):
 	# 	determination = len(life)
 	#	opportunity = 20
 	#	my_purpose = life[inspired*destiny + opportunity % determination]
 
-	if(i_live and still_searching):
+	if i_live and still_searching:
 		print("Another year... another chance")
 		time.sleep(365.25 * days)
 	
-	elif (still_searching):
+	elif still_searching:
 		print("Another life... another chance")
 		begin_life()
 
@@ -53,5 +57,10 @@ def begin_life():
 	destiny = life[chance]		# Need to change chance
 	this_life(destiny)
 
+def still_alive():
+	if(random.random() < health ):
+		return i_live
+	else:
+		return not i_live
 
 begin_life()
